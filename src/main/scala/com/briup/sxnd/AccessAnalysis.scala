69b443a3a1eb5ed8a05e268ae4a4fcc1513a7900
lcc-666 <2502247952@qq.com>
2023-01-07 09:54:09 +0800

    完成代码

--- src/main/scala/com/briup/sxnd/AccessAnalysis.scala
+++ src/main/scala/com/briup/sxnd/AccessAnalysis.scala
@@ -1,6 +1,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 
+import java.sql.DriverManager
 import java.util.Calendar
 
 
@@ -13,8 +14,10 @@ object AccessAnalysis {
 
     val sc = new SparkContext(conf)
 
-        var rdd =sc.textFile("./src/main/scala/com/briup/sxnd/user_defined.log")
+//        var rdd =sc.textFile("./src/main/scala/com/briup/sxnd/user_defined.log")
 //    val rdd = sc.textFile("./src/main/scala/com/briup/sxnd/user_defined_2022-02-15.log")
+      val rdd = sc.textFile(args(0))
+
 
     val allData = rdd.map(mes => {
       val datas = mes.replace("||", ",").split(",")
@@ -25,8 +28,8 @@ object AccessAnalysis {
       calendar.setTimeInMillis(ts.toLong)
       ((calendar.get(Calendar.DAY_OF_MONTH),
 
-        calendar.get(Calendar.HOUR_OF_DAY)),(datas(17),datas(16),datas(19),(ts.toLong)))
-//        calendar.get(Calendar.HOUR_OF_DAY)), (datas(17), datas(15), datas(19), (ts.toLong)))
+//        calendar.get(Calendar.HOUR_OF_DAY)),(datas(17),datas(16),datas(19),(ts.toLong)))
+        calendar.get(Calendar.HOUR_OF_DAY)), (datas(17), datas(15), datas(19), (ts.toLong)))
 
     })
 //    allData.foreach(println)
@@ -93,8 +96,52 @@ object AccessAnalysis {
     val icm = only.rightOuterJoin(all).map{
       case (v1,v2) =>(v1,v2._1.getOrElse(0).toDouble/v2._2.toDouble)
     }
-    icm.foreach(println)
+//    icm.foreach(println)
 
+//    val userAt=cleanData.map(x=>{
+//      (x._2._2,x._2._4)
+//    }).groupByKey().mapValues(x=>x.max-x.min)
+
+    val haat=cleanData.map(x=>{
+      ((x._1,x._2._2),x._2._4)
+    }).groupByKey().mapValues(x=>x.max-x.min)
+      .map(x=>(x._1._1,x._2)).groupByKey().mapValues(x=>x.sum/x.size)
+//    haat.foreach(println)
+
+    val data = hpv.leftOuterJoin(puv)
+      .leftOuterJoin(hip).leftOuterJoin(haat).leftOuterJoin(cv)
+      .leftOuterJoin(icm).map{
+      case (v1,v2)=>(v1,v2._1._1._1._1._1,v2._1._1._1._1._2.getOrElse(0),v2._1._1._1._2.getOrElse(0)
+        ,v2._1._1._2.getOrElse(0),v2._1._2.getOrElse(0),v2._2.getOrElse(0.0))
+    }
+
+    data.foreach(println)
+
+    data.foreachPartition{
+      iter=>{
+        val driver = "com.mysql.jdbc.Driver"
+        val url = "dbc:mysql://localhost:3306/estore3"
+        val user = "briup"
+        val passwd = "briup"
+        Class.forName(driver)
+        val coon =DriverManager.getConnection(url,url,passwd)
+        val sql= "insert into ref_data(day,hour,pv,uv,ip,times,cv,icm) values(?,?,?,?,?,?,?,?)"
+        val prep = coon.prepareStatement(sql)
+        iter.foreach(x=>{
+          prep.setInt(1,x._1._1)
+          prep.setInt(2,x._1._2)
+          prep.setInt(3,x._2)
+          prep.setInt(4,x._3)
+          prep.setInt(5,x._4)
+          prep.setDouble(6,x._5.toDouble)
+          prep.setInt(7,x._6)
+          prep.setDouble(8,x._7.toDouble)
+          prep.execute()
+        })
+        prep.close()
+        coon.close()
+      }
+    }
   }
 
 }
